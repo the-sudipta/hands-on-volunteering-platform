2e38a0cac8f0a0750f819c0333e73362d9bce881
Sudipta Desktop BD <engineer.sudipta.kumar.das@gmail.com>
2025-03-06 20:23:28 +0600

    🛠 config: Add database configurations
    Add database configurations for the PostgreSQL database from Railway app (online version)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-
-
-
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      host: process.env.PGHOST,
+      port: parseInt(process.env.PGPORT || '5432', 10),
+      username: process.env.PGUSER, // process.env => means .env file of your folder
+      password: process.env.PGPASSWORD, //Change to your Password
+      database: process.env.PGDATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
