41701df914bdd0e96d4b92f6ecf60d08e93389d9
Sudipta Desktop BD <engineer.sudipta.kumar.das@gmail.com>
2025-03-15 15:20:22 +0600

    🆕 new: Add new tables and establish relationships in the backend
    Added new tables: Event, Community_Help_Request, Attendee, Comment, Team, and Certificate in the backend. Established necessary relationships among them to ensure proper data integrity and interaction. With this update, all required tables for the project are now completed.

--- backend/src/User/user.entity.ts
+++ backend/src/User/user.entity.ts
@@ -22,11 +22,37 @@ export class UserEntity {
   @Column()
   role: string;
 
+
+  //region Relationships
   @OneToMany(() => SessionEntity, (session) => session.user)
   sessions: SessionEntity[];
 
   @OneToMany(() => OtpEntity, (otp) => otp.user)
   otp: OtpEntity[];
+
+  @OneToMany(() => EventEntity, (event) => event.user)
+  event: EventEntity[];
+
+  @OneToMany(() => Community_Help_RequestEntity, (community_help_request) => community_help_request.user)
+  community_help_request: Community_Help_RequestEntity[];
+
+  @OneToMany(() => AttendeeEntity, (attendee) => attendee.user)
+  attendee: AttendeeEntity[];
+
+  @OneToMany(() => CommentEntity, (comment) => comment.user)
+  comment: CommentEntity[];
+
+  @OneToMany(() => TeamEntity, (team) => team.user)
+  team: TeamEntity[];
+
+ @OneToMany(() => CertificateEntity, (certificate) => certificate.user)
+ certificate: CertificateEntity[];
+
+
+
+  //endregion Relationships
+
+
 }
 
 @Entity('profile')
@@ -91,5 +117,128 @@ export class OtpEntity {
   expiration_date: string;
 
   @ManyToOne(() => UserEntity, (user) => user.otp)
+  user: UserEntity;
+}
+
+
+@Entity('event')
+export class EventEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  category: string;
+
+  @Column()
+  location: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  time: string;
+
+  @ManyToOne(() => UserEntity, (user) => user.event)
+  user: UserEntity;
+
+  @OneToMany(() => AttendeeEntity, (attendee) => attendee.event)
+  attendee: AttendeeEntity[];
+
+}
+
+@Entity('community_help_request')
+export class Community_Help_RequestEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  status: string;
+
+  @Column()
+  description: string;
+
+
+  @ManyToOne(() => UserEntity, (user) => user.community_help_request)
+  user: UserEntity;
+
+  @OneToMany(() => CommentEntity, (comment) => comment.community_help_request)
+  comment: CommentEntity[];
+
+
+}
+
+@Entity('attendee')
+export class AttendeeEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  start_time: string;
+
+  @Column()
+  end_time: string;
+
+
+  @ManyToOne(() => UserEntity, (user) => user.attendee)
+  user: UserEntity;
+
+  @ManyToOne(() => EventEntity, (event) => event.attendee)
+  event: EventEntity;
+}
+
+@Entity('comment')
+export class CommentEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  text: string;
+
+  @Column()
+  time: string;
+
+
+  @ManyToOne(() => UserEntity, (user) => user.comment)
+  user: UserEntity;
+
+  @ManyToOne(() => Community_Help_RequestEntity, (community_help_request) => community_help_request.comment)
+  community_help_request: Community_Help_RequestEntity;
+}
+
+@Entity('team')
+export class TeamEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  type: string;
+
+  @Column()
+  task_description: string;
+
+
+  @ManyToOne(() => UserEntity, (user) => user.team)
+  user: UserEntity;
+}
+
+@Entity('certificate')
+export class CertificateEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+
+  @ManyToOne(() => UserEntity, (user) => user.certificate)
   user: UserEntity;
 }
